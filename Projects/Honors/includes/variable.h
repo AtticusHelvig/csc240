b794f27c80fcd4907fe01769c73c1314c4c47fe1
Atticus Helvig <ajhelvig@proton.me>
2024-02-26 22:06:33 -0700

    Typedef Variable

--- Projects/Honors/includes/variable.h
+++ Projects/Honors/includes/variable.h
@@ -1,10 +1,10 @@
 #ifndef VARIABLE_H
 #define VARIABLE_H
 
-struct Variable
-{
-    char *id;
-    int value;
+typedef struct Variable var;
+struct Variable {
+  char *id;
+  int *value;
 };
 
 int var_isIdEqual(const struct Variable *variable, const char *id);
