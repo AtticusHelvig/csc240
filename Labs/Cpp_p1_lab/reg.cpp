3ccbc425c3be62d95b5546266749fd3cb3f85c94
Atticus Helvig <ajhelvig@proton.me>
2024-03-19 21:25:33 -0700

    Add Tesla and printStudent function

--- Labs/Cpp_p1_lab/reg.cpp
+++ Labs/Cpp_p1_lab/reg.cpp
@@ -1,25 +1,44 @@
 #include <iostream>
+
 #include "student.h"
 
 using namespace std;
 
+void printStudent(Student *student);
+
 int main() {
-   Student snoopy("Joe Cool");
+    Student snoopy("Joe Cool");
 
-   snoopy.addClass(3, 4);
-   snoopy.addClass(4, 3);
-   snoopy.addClass(3, 2);
-   snoopy.addClass(5, 4);
+    snoopy.addClass(3, 4);
+    snoopy.addClass(4, 3);
+    snoopy.addClass(3, 2);
+    snoopy.addClass(5, 4);
 
-   cout << "Name: " << snoopy.getName() << endl;
-   cout << "ID: " << snoopy.getID() << endl;
-   printf("GPA: %4.2f\n", snoopy.calcGPA());
+    cout << "Name: " << snoopy.getName() << endl;
+    cout << "ID: " << snoopy.getID() << endl;
+    printf("GPA: %4.2f\n", snoopy.calcGPA());
 
-   cout << endl;
+    cout << endl;
 
-   // Add Nikola Tesla Here
+    // Add Nikola Tesla Here
+    Student tesla("Nikola Tesla");
 
-   // Add yourself as a pointer to s Student with the new operator.
+    tesla.addClass(3, 4);
+    tesla.addClass(4, 4);
+    tesla.addClass(3, 4);
+    tesla.addClass(5, 4);
+    tesla.addClass(4, 4);
+    tesla.addClass(3, 4);
+    printStudent(&tesla);
 
-   return 0;
+    // Add yourself as a pointer to s Student with the new operator.
+
+    return 0;
+}
+
+void printStudent(Student *student) {
+    cout << "Name: " << student->getName() << endl;
+    cout << "ID: " << student->getID() << endl;
+    printf("GPA: %4.2f\n", student->calcGPA());
+    cout << endl;
 }
